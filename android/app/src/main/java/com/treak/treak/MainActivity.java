4a19bc5e417785596cae1c61fa3433cc9dd74d5f
dhalima3 <dhalima@gatech.edu>
2015-10-03 06:52:54 -0500

    set up parse

--- android/app/src/main/java/com/treak/treak/MainActivity.java
+++ android/app/src/main/java/com/treak/treak/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +15,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "4e7lcr27mzMidGqPcG69BcUlsgrwDkqSuKbc5uAp", "agshqO6DFFDkozhxroFRtpDWiJUppLTdHfliEIdL");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 
     @Override
