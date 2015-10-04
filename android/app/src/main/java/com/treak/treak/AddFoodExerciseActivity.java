73474240866fe7c34bd1db48d1ef8038ff2484df
dhalima3 <dhalima@gatech.edu>
2015-10-04 09:59:20 -0500

    consolidate food and exercise add screens

--- android/app/src/main/java/com/treak/treak/AddFoodExerciseActivity.java
+++ android/app/src/main/java/com/treak/treak/AddFoodExerciseActivity.java
@@ -5,17 +5,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import com.treak.treak.helpers.UtilityMethods;
 import com.treak.treak.models.ExerciseItem;
-import com.treak.treak.models.User;
+import com.treak.treak.models.FoodItem;
 
-public class AddExerciseActivity extends ActionBarActivity {
+public class AddFoodExerciseActivity extends ActionBarActivity {
 
     EditText exerciseValue;
+    EditText foodValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +21,7 @@ public class AddExerciseActivity extends ActionBarActivity {
         setContentView(R.layout.activity_add_exercise);
 
         exerciseValue = (EditText) findViewById(R.id.exercise_name_value);
+        foodValue = (EditText) findViewById(R.id.food_name_value);
     }
 
     @Override
@@ -42,14 +41,24 @@ public class AddExerciseActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        } else if (id == R.id.action_save_exercise_details) {
-            String name = exerciseValue.getText().toString();
-            if (name != null) {
-                ExerciseItem exerciseItem = new ExerciseItem(name);
+        } else if (id == R.id.action_save_details) {
+            String exerciseName = exerciseValue.getText().toString();
+            String foodName = foodValue.getText().toString();
+            if (!exerciseName.isEmpty()) {
+                ExerciseItem exerciseItem = new ExerciseItem(exerciseName);
                 exerciseItem.saveInBackground();
                 finish();
-            } else {
+                Toast.makeText(getApplicationContext(), "Details saved to Parse", Toast.LENGTH_SHORT).show();
             }
+
+            if (!foodName.isEmpty()) {
+                FoodItem foodItem = new FoodItem(foodName);
+                foodItem.saveInBackground();
+                finish();
+                Toast.makeText(getApplicationContext(), "Details saved to Parse", Toast.LENGTH_SHORT).show();
+            }
+
+            Toast.makeText(getApplicationContext(), "Please type in valid data", Toast.LENGTH_SHORT).show();
         }
 
         return super.onOptionsItemSelected(item);
